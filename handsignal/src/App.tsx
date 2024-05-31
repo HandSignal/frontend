e43a070cd3c6e7235f57b6d8197ea357605e2f02
kittyismylife <chss0520@sookmyung.ac.kr>
2024-05-31 17:57:14 +0900

    Feat: 손 인식 완료

--- handsignal/src/App.tsx
+++ handsignal/src/App.tsx
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+import { css } from "@emotion/css";
+import { Camera } from "@mediapipe/camera_utils";
+import { Hands, Results } from "@mediapipe/hands";
+import { drawCanvas } from "./utils/drawCanvas";
 
-function App() {
+const App = () => {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const resultsRef = useRef<Results>();
+
+  /**
+   * 검출결과（프레임마다 호출됨）
+   * @param results
+   */
+  const onResults = useCallback((results: Results) => {
+    resultsRef.current = results;
+
+    const canvasCtx = canvasRef.current!.getContext("2d")!;
+    drawCanvas(canvasCtx, results);
+  }, []);
+
+  // 초기 설정
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+
+    hands.setOptions({
+      maxNumHands: 2, // 손을 몇 개 까지 인식할지
+      modelComplexity: 1, // 모델 복잡도 → 모델이 얼마나 다양하고 상세한 패턴을 학습하는지 
+      minDetectionConfidence: 0.5, // 최소 신뢰도 → 모델이 인식한 결과 중 신뢰도가 0.5 이상인 결과만 출력
+      minTrackingConfidence: 0.5, // 최대 신뢰도
+    });
+
+    hands.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      const camera = new Camera(webcamRef.current.video!, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current!.video! });
+        },
+        width: 1280,
+        height: 720,
+      });
+      camera.start();
+    }
+  }, [onResults]);
+
+  /*  랜드마크들의 좌표를 콘솔에 출력 */
+  const OutputData = () => {
+    const results = resultsRef.current!;
+    console.log(results.multiHandLandmarks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      {/* 비디오 캡쳐 */}
+      <Webcam
+        audio={false}
+        style={{ visibility: "hidden" }}
+        width={1280}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{ width: 1280, height: 720, facingMode: "user" }}
+      />
+      {/* 랜드마크를 손에 표시 */}
+      <canvas
+        ref={canvasRef}
+        className={styles.canvas}
+        width={1280}
+        height={720}
+      />
+      {/* 좌표 출력 */}
+      <div className={styles.buttonContainer}>
+        <button className={styles.button} onClick={OutputData}>
+          Output Data
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+// ==============================================
+// styles
+
+const styles = {
+  container: css`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  canvas: css`
+    position: absolute;
+    width: 1280px;
+    height: 720px;
+    background-color: #fff;
+  `,
+  buttonContainer: css`
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  `,
+  button: css`
+    color: #fff;
+    background-color: #0082cf;
+    font-size: 1rem;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 10px;
+    cursor: pointer;
+  `,
+};
 
 export default App;
